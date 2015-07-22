e847f543f1454793d7284824bd2ea8ffdef65791
kyzya30 <kyzya30@inbox.ru>
2015-07-22 21:07:08 +0300

    commit 1

---
+++ DivNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divider_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value:");
+            int value = int.Parse(Console.ReadLine());
+            Console.WriteLine("Result:");
+            for (int i = 1; i <= value; i++)
+            {
+                if (value % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
